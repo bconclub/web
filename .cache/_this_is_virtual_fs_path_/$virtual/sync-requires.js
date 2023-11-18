278b973b76cab930e1e1ddb6bd817f2b47dfc2fc
bcon <bconclubx@gmail.com>
2023-11-18 10:28:18 +0530

    mm

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,17 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\tejas\\OneDrive\\Documents\\GitHub\\web\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\tejas\\OneDrive\\Documents\\GitHub\\web\\src\\pages\\404.js")),
+  "component---src-pages-about-js": preferDefault(require("C:\\Users\\tejas\\OneDrive\\Documents\\GitHub\\web\\src\\pages\\about.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\tejas\\OneDrive\\Documents\\GitHub\\web\\src\\pages\\index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("C:\\Users\\tejas\\OneDrive\\Documents\\GitHub\\web\\src\\pages\\page-2.js")),
+  "component---src-pages-services-js": preferDefault(require("C:\\Users\\tejas\\OneDrive\\Documents\\GitHub\\web\\src\\pages\\services.js")),
+  "component---src-pages-using-ssr-js": preferDefault(require("C:\\Users\\tejas\\OneDrive\\Documents\\GitHub\\web\\src\\pages\\using-ssr.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("C:\\Users\\tejas\\OneDrive\\Documents\\GitHub\\web\\src\\pages\\using-typescript.tsx")),
+  "component---src-templates-using-dsg-js": preferDefault(require("C:\\Users\\tejas\\OneDrive\\Documents\\GitHub\\web\\src\\templates\\using-dsg.js"))
+}
+
